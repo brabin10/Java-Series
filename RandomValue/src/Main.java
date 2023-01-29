4a2efe3b943b7547dabb84393e451b797c9e2a2a
Rabin Basnet <rowbinr@yahoo.com>
2023-01-28 21:40:03 -0700

    playing with random numbers is fun

---
+++ RandomValue/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		//int x = random.nextInt(6)+1; // If we want num through 1 to 6, we could just add 1
+		//double y = random.nextDouble();
+		boolean z = random.nextBoolean();
+		 
+		System.out.println(z);
+	}
+
+}
