d70beb9e9ed0975c01619380eeb7fe5cd32c5721
Rabin Basnet <rowbinr@yahoo.com>
2023-01-28 21:53:02 -0700

    working with If stmt

---
+++ IfStatement/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		// if statement = performs a block of code if it's condition evaluates to be true
+		
+		int age = 78;
+		
+		if(age>=20) {
+			System.out.println("Ok Boomer!");
+			
+		}
+		else if (age>=78) {
+			System.out.println("You are an adult");
+		}
+		else {
+			System.out.println("Your are not an adult!");
+		}
+	}
+
+}
