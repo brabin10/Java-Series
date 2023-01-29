c3dfcbc791f82fe3a9e90e0ff24ed1d1177819eb
Rabin Basnet <rowbinr@yahoo.com>
2023-01-28 22:06:42 -0700

    switch statement

--- IfStatement/src/Main.java
+++ IfStatement/src/Main.java
@@ -11,8 +11,11 @@ public class Main {
 			System.out.println("Ok Boomer!");
 			
 		}
-		else if (age>=78) {
+		else if (age>=18) {
 			System.out.println("You are an adult");
+		}
+		else if(age>=13) {
+			System.out.println("Your are  a teenager!");
 		}
 		else {
 			System.out.println("Your are not an adult!");
